1533a1bbe66fd9a7f66147e9dfc691620975620c
Halecs <reyesjimenez22@gmail.com>
2018-10-27 23:58:40 +0200

    ahora

--- Partida.cpp
+++ Partida.cpp
@@ -5,15 +5,6 @@
 int Partida::descubrirCasilla(std::string letra,int j){
    int i = letraNumero(letra);
    int resultado= busc_.descubrirCasilla(i,j);
-   if(resultado==1)
-   {
-        jugadores_[0].setEstado(REGISTRADO_SIN_PARTIDA);
-        jugadores_[1].setEstado(REGISTRADO_SIN_PARTIDA);
-        return 1;
-    }
-  
-   else if(resultado==-1) return -1;
-   else{    turno_=(turno_+1)%2; return 0;}
    turno_=(turno_+1)%2;
    int resultado = busc_.descubrirCasilla(i,j);
   if(resultado == 1)
